2b5451d5e029183b538973c902f8d9e4d325b9b7
YarinVanZikri <yarinvanzikri@gmail.com>
2021-08-02 14:32:41 +0300

    Added Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import Initials from "./Initials/Initials";
+import DayOfWeek from "./DayOfWeek/DayOfWeek"
+import Article from "./Article/Article";
+import Menu from "./Menu/Menu"
+import MenuItem from "./Menu/MenuItem/MenuItem"
 import './App.css';
+import Counter from "./Counter/Counter";
+import StudentPicker from "./StudentPicker/StudentPicker";
+import RandomColorSquare from "./RandomColorSquare/RandomColorSquare";
 
 function App() {
+    function clickHandler() {
+        console.log('you clicked');
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {/*<button onClick={clickHandler}>Click me</button>*/}
+        {/*<Counter />*/}
+        {/*<StudentPicker/>*/}
+        <div>
+            <Menu>
+                <h1>Menu</h1>
+                <MenuItem>
+                    <a
+                        href={'/Content'}>
+                        Content
+                    </a>
+                </MenuItem>
+                <MenuItem>
+                    <a
+                        href={'/Gallery'}>
+                        Gallery
+                    </a>
+                </MenuItem>
+                <MenuItem>
+                    <a href={'/Contact'}>
+                        Contact
+                    </a>
+                </MenuItem>
+            </Menu>
+        </div>
+        <div>
+        <RandomColorSquare>
+
+        </RandomColorSquare>
+        </div>
+      {/*<Initials name='Yarin Vanzikri'/>*/}
+      {/*<DayOfWeek day={1}/>*/}
+      {/*  <Article title='Weather' Content='The Weather Is Changing, What Do You Think?'/>*/}
     </div>
-  );
+  )
 }
 
 export default App;
